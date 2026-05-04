38aca9c6b7dfda0d6fc3999936f36c947a129a00
Kawausa <qualification07@gmail.com>
2026-05-04 20:17:54 +0900

    Create drone.h

---
+++ Result/drone.h
@@ -0,0 +1,47 @@
+#ifndef NODE_H
+#define NODE_H
+
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <time.h>
+#include <windows.h>
+#include <stdbool.h>
+
+#define Battery_per_move
+#define Enemy_spotted 8
+#define Bomb 1
+#define Map_size 100
+
+typedef struct Node
+{
+	struct Node* rlink;
+	struct Node* llink;
+	int x, y;
+	int link_pos;
+	//int is_blocked;
+	char* content;
+} Node;
+
+typedef struct Stack
+{
+	int x, y;
+	int link_pos;
+	struct Stack* next;
+} Stack;
+
+
+extern int block_pos[100][2];
+extern int block_cnt;
+
+void start_UI();
+void input_sys();
+void sort_nodes();
+void get_node_nums();
+void do_random();
+void do_direct();
+void do_FILE();
+
+
+#endif
