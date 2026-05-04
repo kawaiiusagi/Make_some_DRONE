90edae8a2c52c376e814920f9db2bb24fbf0b6ea
truth-star <mb040319@gmail.com>
2026-05-04 20:12:41 +0900

    Create pipeNode.h

---
+++ MB/pipeNode.h
@@ -0,0 +1,8 @@
+void print01(Node_h* Hnode); //Node_h는 헤더 
+void print02(Node_h* Hnode, Node* pre); //01.txt를 복사 붙여넣기 하기?
+void print03(Calc_h* cacl); //Calc_h는 거리 및 배터리소모가 계산되어 저장된 노드의 헤더
+void print04(Last_h* last); 
+//Last_h는 Calc_h에 누적 소모 배터리, 상태, 이벤트, 안전 경로까지 있는 노드의 헤더
+void read01(FILE* fr); //01.txt를 복사 붙여넣기 하기?
+void read02(FILE* fr); //02.txt를 읽어 노드 만들기
+void read03(FILE* fr); //03.txt를 읽어 새 노드 만들기
