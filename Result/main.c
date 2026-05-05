089293db6c7927180aeebf2cb9a3d541d7354298
Kawausa <qualification07@gmail.com>
2026-05-06 02:15:56 +0900

    Update main.c

--- Result/main.c
+++ Result/main.c
@@ -4,17 +4,10 @@ int main()
 {
 	start_UI();
 
-	input_sys();
-
 	read_list();
 
 	read_node();
 
-	build_dist_list();
-	
-	print_dist_list();
-
-	fread03();
-	reporting();
+	make_final();
 	return 0;
 }
